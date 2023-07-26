2798c69746c0bd4d901e48b177d62699acdc7c68
Florian Novak <133348117+FlorianNova@users.noreply.github.com>
2023-07-26 14:01:01 +0200

    added scroll top/bottom

--- components/PokemonList.js
+++ components/PokemonList.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import PokemonCard from './PokemonCard';
 import fetchPokemonData from '../services/fetchPokemonData';
 import styled from 'styled-components';
+import { animateScroll as scroll } from 'react-scroll';
 
 const GridWrapper = styled.div`
   display: grid;
@@ -9,7 +10,7 @@ const GridWrapper = styled.div`
   gap: 16px;
 
   @media (max-width: 384px) {
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(1, 1fr);
   }
 `;
 
@@ -23,19 +24,46 @@ const SearchBar = styled.input`
   display: block;
   margin: 0 auto;
   margin-bottom: 7%;
-  background-color: #
-
+  background-color: #caf6d6;
 
   @media (max-width: 3840px) {
     width: 60vw;
   }
 `;
 
+const ScrollToTopButton = styled.button`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  display: ${(props) => (props.visible ? 'block' : 'none')};
+  cursor: pointer;
+`;
 
+const ScrollToBottomButton = styled.button`
+  position: fixed;
+  bottom: 20px; /* Updated to 20px */
+  left: 20px; /* Added left positioning */
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  display: ${(props) => (props.visible ? 'block' : 'none')};
+  cursor: pointer;
+`;
 
 export default function PokemonList() {
   const [pokemonData, setPokemonData] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [showScrollToTop, setShowScrollToTop] = useState(false);
+  const [showScrollToBottom, setShowScrollToBottom] = useState(false);
 
   useEffect(() => {
     const fetchPokemon = async () => {
@@ -47,6 +75,29 @@ export default function PokemonList() {
       }
     };
     fetchPokemon();
+
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const screenHeight = window.innerHeight;
+      const fullHeight = document.documentElement.scrollHeight;
+
+      if (scrollY > 300) {
+        setShowScrollToTop(true);
+      } else {
+        setShowScrollToTop(false);
+      }
+
+      if (fullHeight - scrollY - screenHeight > 300) {
+        setShowScrollToBottom(true);
+      } else {
+        setShowScrollToBottom(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, []);
 
   const filteredPokemon = pokemonData.filter((pokemon) => {
@@ -55,6 +106,14 @@ export default function PokemonList() {
       pokemon.id.toString().includes(searchTerm)
     );
   });
+
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
+  const scrollToBottom = () => {
+    scroll.scrollToBottom();
+  };
 
   return (
     <div>
@@ -74,6 +133,20 @@ export default function PokemonList() {
           />
         ))}
       </GridWrapper>
+      <ScrollToTopButton
+        visible={showScrollToTop}
+        onClick={scrollToTop}
+        title="Scroll to Top"
+      >
+        &#8593;
+      </ScrollToTopButton>
+      <ScrollToBottomButton
+        visible={showScrollToBottom}
+        onClick={scrollToBottom}
+        title="Scroll to Bottom"
+      >
+        &#8595;
+      </ScrollToBottomButton>
     </div>
   );
 }
