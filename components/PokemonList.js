978fb6f62697e4bdb3f761459f64646f98344eba
Florian Novak <133348117+FlorianNova@users.noreply.github.com>
2023-07-26 13:17:44 +0200

    added Searchbar & functionality

--- components/PokemonList.js
+++ components/PokemonList.js
@@ -8,13 +8,34 @@ const GridWrapper = styled.div`
   grid-template-columns: repeat(3, 1fr);
   gap: 16px;
 
-  @media (max-width: 768px) {
+  @media (max-width: 384px) {
     grid-template-columns: repeat(3, 1fr);
   }
 `;
 
+const SearchBar = styled.input`
+  width: 50vw;
+  padding: 12px;
+  font-size: 2vw;
+  border: 3px solid #ddd;
+  border-radius: 20px;
+  margin-bottom: 16px;
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 7%;
+  background-color: #
+
+
+  @media (max-width: 3840px) {
+    width: 60vw;
+  }
+`;
+
+
+
 export default function PokemonList() {
   const [pokemonData, setPokemonData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchPokemon = async () => {
@@ -28,16 +49,31 @@ export default function PokemonList() {
     fetchPokemon();
   }, []);
 
+  const filteredPokemon = pokemonData.filter((pokemon) => {
+    return (
+      pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      pokemon.id.toString().includes(searchTerm)
+    );
+  });
+
   return (
-    <GridWrapper>
-      {pokemonData.map((pokemon) => (
-        <PokemonCard
-          key={pokemon.id}
-          name={pokemon.name}
-          number={pokemon.id}
-          imageUrl={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
-        />
-      ))}
-    </GridWrapper>
+    <div>
+      <SearchBar
+        type="text"
+        placeholder="Search by name or number..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <GridWrapper>
+        {filteredPokemon.map((pokemon) => (
+          <PokemonCard
+            key={pokemon.id}
+            name={pokemon.name}
+            number={pokemon.id}
+            imageUrl={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+          />
+        ))}
+      </GridWrapper>
+    </div>
   );
 }
